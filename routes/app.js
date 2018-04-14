dc7cf48dd808f4c53e23a35dffecc0fed8dc0d34
Christopher Ford <christopher@Christophers-Mac-mini.local>
2018-04-14 17:18:18 +0100

    Some initial code for the node refresher

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('index');
+router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/message/:msg', (req, res, next) => res.render('node', { message: req.params.msg }));
+
+router.post('/message', (req, res, next) => {
+    const message = req.body.message;
+
+    res.redirect(`/message/${message}`);
 });
 
 module.exports = router;
