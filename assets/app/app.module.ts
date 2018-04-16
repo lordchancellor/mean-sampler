b9b4e00042ab6da6d2ee061c18c8211fb7171d64
Christopher Ford <c.d.j.ford@gmail.com>
2018-04-16 13:53:41 +0100

    Added Angular routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { routing } from './app.routing';
+
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './header.component';
+
 import { MessagesComponent } from './messages/messages.component';
 import { MessageComponent } from './messages/message.component';
 import { MessageListComponent } from './messages/message-list.component';
 import { MessageInputComponent } from './messages/message-input.component';
+
+import { AuthenticationComponent } from './auth/authentication.component';
 
 const messageComponents: any[] = [
     MessagesComponent,
@@ -15,14 +22,21 @@ const messageComponents: any[] = [
     MessageInputComponent
 ];
 
+const authComponents: any[] = [
+    AuthenticationComponent
+]
+
 @NgModule({
     declarations: [
         AppComponent,
-        messageComponents
+        HeaderComponent,
+        messageComponents,
+        authComponents
     ],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        routing
     ],
     bootstrap: [
         AppComponent
