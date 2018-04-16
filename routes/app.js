8741cfce0f80be239f51b7ad17b7fa1c70dbfa67
Christopher Ford <c.d.j.ford@gmail.com>
2018-04-16 13:36:52 +0100

    Changes to server files in preparation for frontend

--- routes/app.js
+++ routes/app.js
@@ -1,29 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/user');
 
-router.get('/', (req, res, next) => {
-    User.findOne({}, (err, doc) => {
-        if (err) {
-            return res.send('error');
-        }
-
-        res.render('node', { email: doc.email });
-    });
-});
-
-router.post('/', (req, res, next) => {
-    const email = req.body.email;
-    const user = new User({
-        firstName: 'Chris',
-        lastName: 'Ford',
-        password: 'Password1',
-        email
-    });
-
-    user.save();
-
-    res.redirect(`/`);
-});
+router.get('/', (req, res, next) => res.render('index'));
 
 module.exports = router;
